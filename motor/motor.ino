170789f92f5d1ca3d7145b26fae5df015341c8e0
Justin Peng <justinpeng98@gmail.com>
2022-06-28 20:58:34 +0800

    feat: motor controlling with joystick

---
+++ motor/motor.ino
@@ -0,0 +1,94 @@
+// Motor A connections
+int enA = 9;
+int dirA1 = 8;
+int dirA2 = 7;
+// Motor B connections
+int enB = 3;
+int dirB1 = 5;
+int dirB2 = 4;
+
+// Joystick
+int jPinUpDown = A1;
+int jPinLeftRight = A2;
+int jVal;
+int jVal2;
+int mSpeed;
+int MAXSPEED = 50.0;
+
+void setup() {
+  // Set Joystick input
+  pinMode(jPinUpDown,INPUT);
+  pinMode(jPinLeftRight,INPUT);
+  // Set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(dirA1, OUTPUT);
+  pinMode(dirA2, OUTPUT);
+  pinMode(dirB1, OUTPUT);
+  pinMode(dirB2, OUTPUT);
+  
+  // Turn off motors - Initial state
+  digitalWrite(dirA1, LOW);
+  digitalWrite(dirA2, LOW);
+  digitalWrite(dirB1, LOW);
+  digitalWrite(dirB2, LOW);
+  Serial.begin(9600); // for the serial monitor
+}
+
+void loop() {
+  jVal = analogRead(jPinUpDown);
+  jVal2 = analogRead(jPinLeftRight);
+//  Serial.println(jVal);
+  Serial.println(jVal2);
+//  Serial.println("Vertical axis: "+jVal);
+//  Serial.println("Horizontal axis: "+jVal2);
+  
+  // joystick stick is forward
+  if (jVal <512){
+    mSpeed = -MAXSPEED/512.*jVal+MAXSPEED;
+    moveForward(mSpeed);
+  }
+  
+  // joystick is back
+  if(jVal >= 512){
+    mSpeed = (MAXSPEED/512.)*jVal-MAXSPEED;
+    moveBackward(mSpeed);
+  }
+
+}
+
+void moveForward(int val){
+    digitalWrite(dirA1,LOW);
+    digitalWrite(dirA2,HIGH);
+    digitalWrite(dirB1,LOW);
+    digitalWrite(dirB2,HIGH);
+    analogWrite(enA,val);
+    analogWrite(enB,val);
+}
+
+void moveBackward(int val){
+    digitalWrite(dirA1,HIGH);
+    digitalWrite(dirA2,LOW);
+    digitalWrite(dirB1,HIGH);
+    digitalWrite(dirB2,LOW);
+    analogWrite(enA,val);
+    analogWrite(enB,val);
+}
+
+void moveLeft(int val){
+    digitalWrite(dirA1,HIGH);
+    digitalWrite(dirA2,LOW);
+    digitalWrite(dirB1,LOW);
+    digitalWrite(dirB2,LOW);
+    analogWrite(enA,val);
+//    analogWrite(enB,val);
+}
+
+void moveRight(int val){
+    digitalWrite(dirA1,LOW);
+    digitalWrite(dirA2,LOW);
+    digitalWrite(dirB1,HIGH);
+    digitalWrite(dirB2,LOW);
+//    analogWrite(enA,val);
+    analogWrite(enB,val);
+}
